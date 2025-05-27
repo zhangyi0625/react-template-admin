427f447cf6287c6aeec8244c2a7c90bfdbacf374
张易 <zhangyi@zaicang.com>
2025-05-27 16:13:34 +0800

    fix:react-router warning about `React.startTransition` in v7

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,30 @@
-import { createRoot } from 'react-dom/client';
-import '@/styles/global.scss'; // 引入 Sass 文件
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { persistor, store } from '@/stores/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Spin } from 'antd';
-import GlobalConfigProvider from './GlobalConfigProvider';
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import '@/styles/global.scss' // 引入 Sass 文件
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { persistor, store } from '@/stores/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Spin } from 'antd'
+import GlobalConfigProvider from './GlobalConfigProvider'
+import './index.css'
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')
 if (container) {
-  const root = createRoot(container);
+  const root = createRoot(container)
   root.render(
     <Provider store={store}>
       <PersistGate loading={<Spin />} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
           <GlobalConfigProvider />
         </BrowserRouter>
       </PersistGate>
-    </Provider>,
-  );
+    </Provider>
+  )
 } else {
-  console.error('Root element not found');
+  console.error('Root element not found')
 }

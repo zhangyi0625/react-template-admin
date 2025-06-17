63a6d7be553bb6175cd24ea14c743902fc605f7e
张易 <zhangyi@zaicang.com>
2025-06-17 15:29:01 +0800

    fix：Format padStart for string dates

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -16,9 +16,9 @@ export function formatTime(time: string | number, format: string) {
     arr = [],
     date = new Date(time)
   arr.push(date.getFullYear()) //Y
-  arr.push(date.getMonth() + 1) //M
-  arr.push(date.getDate()) //D
-  arr.push(date.getHours()) //h
+  arr.push((date.getMonth() + 1).toString().padStart(2, '0')) //M
+  arr.push(date.getDate().toString().padStart(2, '0')) //D
+  arr.push(date.getHours().toString().padStart(2, '0')) //h
   arr.push(date.getMinutes().toString().padStart(2, '0')) //m
   arr.push(date.getSeconds().toString().padStart(2, '0')) //s
   for (var i = 0; i < arr.length; i++) {

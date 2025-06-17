b41246aba5c7a0ac4abe4b343419bf082df09f20
张易 <zhangyi@zaicang.com>
2025-06-17 15:47:09 +0800

    feat：tools add new fun 'replaceObjectName'

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -13,3 +13,26 @@ export function filterKeys(source: any, keys: string[], invert?: boolean) {
       return res
     }, {})
 }
+
+/**
+ * 根据原有的属性名代替新属性名 根据index匹配 两者长度必须匹配
+ * @param arr
+ * @param wishName
+ * @param newName
+ * @returns
+ */
+export function replaceObjectName(
+  arr: any,
+  wishName: string[],
+  newName: string[]
+) {
+  if (wishName.length !== newName.length) return
+  let refreshArr = []
+  arr.map((obj: any) => {
+    wishName.map((name: string, index: number) => {
+      if (obj[name]) obj[newName[index]] = obj[name]
+    })
+  })
+  refreshArr = JSON.parse(JSON.stringify(arr))
+  return refreshArr
+}

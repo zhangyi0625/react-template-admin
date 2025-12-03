eccc13e0e6ccf16e57ae0993357675eb46b8e1b9
张易 <2522248305@qq.com>
2025-12-03 09:43:29 +0800

    refactor：remove the relevant apis and components of cabinManage

--- src/views/cabinManage/OoclTask/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CabinTaskTemplate from '../CabinTaskTemplate';
-
-const OoclTaskTem: React.FC = () => {
-  return <CabinTaskTemplate key="OOCL" carrier="OOCL" setting={null} />;
-};
-
-export default OoclTaskTem;

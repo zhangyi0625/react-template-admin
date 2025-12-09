4df0a8609cd0dc0f34999a2f164d706c5cc3f618
张易 <2522248305@qq.com>
2025-12-09 14:21:30 +0800

    fix：path.resolve

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -51,7 +51,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, 'src'),
+      '@components': 'src/components',
     },
     aliasStrategy: 'prefer-tsconfig',
   },

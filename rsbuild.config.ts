f80a75c65290483908b78b78a4e15d93be0cea4c
张易 <2522248305@qq.com>
2026-01-04 10:58:27 +0800

    fix：adjust "selectFileldName" to "selectFieldName" In the api chore：new menu related to the box flashing function and some functions (interfaces not connected)

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   // 配置html模板
   html: {
     favicon: path.resolve(__dirname, './src/assets/images/favicon.ico'),
-    title: '在舱光速抢舱管理平台',
+    title: '在舱VIP管理平台',
     tags: [
       {
         tag: 'html',

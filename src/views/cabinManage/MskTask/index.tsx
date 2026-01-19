7935128694ce528a3973f61438972b621b7a2881
张易 <2522248305@qq.com>
2026-01-19 14:07:52 +0800

    chore：remove the business module code that is not involved in this branch

--- src/views/cabinManage/MskTask/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CabinTaskTemplate from '../CabinTaskTemplate';
-
-const MskTaskTem: React.FC = () => {
-  return <CabinTaskTemplate key="MSK" carrier="MSK" setting={null} />;
-};
-
-export default MskTaskTem;
